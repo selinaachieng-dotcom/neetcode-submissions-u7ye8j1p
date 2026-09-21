2d5649cef3a6c33d13fe788e3deb4cf3a35e1716
selinaachieng-dotcom <selina.achieng@mpesafoundationacademy.ac.ke>
2026-09-21 14:42:34 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+   def isAnagram(self, s: str, t:  str) -> bool:
+      count = {}
+      count1 = {}
+      for char in s :
+         if char in count:
+             count [char] += 1
+         else:
+             count [char]=1
+
+      for char in t:
+         if char in count1:
+             count1 [char] +=1
+         else:
+             count1[char]=1
+
+      if count == count1:
+         return True
+      else:
+         return False
+
+        
